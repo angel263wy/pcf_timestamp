71c16b97afcc15952e93f97e3f2258dae8827bc1
wangyi-T440 <wangyi>
2021-01-09 18:23:04 +0800

    增加数据回卷去重功能

--- pcf.py
+++ pcf.py
@@ -5,10 +5,10 @@ import os
 
 # ---参数设置部分-----
 dpc_file = 'dpc.csv'
-posp_file = 'posp.csv'
+posp_file = 'posp.xls'
 
 # 轨道计数
-orbit_dpc = 8
+orbit_dpc = 13
 
 
 # POSP轨道 用dpc计数转hex后获得
@@ -27,17 +27,37 @@ pd_dpc = pd.read_csv(dpc_file, header=0, sep=',')  # encoding='GB2312'
 # pd_dpc =  pd.read_csv(dpc_file, header=0, sep='\t', encoding='GB2312')  # encoding='GB2312'
 
 ## POSP UTF-8 CSV格式
-pd_posp = pd.read_csv(posp_file, header=0, sep=',')  # encoding='GB2312'
+# pd_posp = pd.read_csv(posp_file, header=0, sep=',')  # encoding='GB2312'
 ## POSP GB2312格式 解包软件直接导出
-# pd_posp =  pd.read_csv(posp_file, header=0, sep='\t', encoding='GB2312')  # encoding='GB2312'
+pd_posp =  pd.read_csv(posp_file, header=0, sep='\t', encoding='GB2312')  # encoding='GB2312'
 
 
 # --------DPC数据计算--------
 # 切片取出用于计算的数据
+print('--------DPC数据处理开始--------')
 dpc_time = pd_dpc.loc[:, ['运行轨序号', '帧计数', 'GPS秒脉冲整秒时间码', '秒脉冲内自守时计数', '一圈内图像序号']]
 
 # 挑选数据 选轨道号 选偶数帧
 dpc_time = dpc_time[dpc_time['运行轨序号']  == orbit_dpc ]
+# 重新索引 方便去回卷
+dpc_time.index = np.arange(len(dpc_time['运行轨序号']))
+# 去除回卷数据
+''' 算法说明
+i为当前帧计数指针 pt_index为上次帧计数指针 
+如果两个指针帧计数之差为1 则连续 更新上次指针 
+如果之差小于1 则表示有回卷 删除i行 pt不变 i继续增加 重新判断 
+如果之差大于1 则表示有丢帧 提示后不处理
+'''
+pt_index = 0 # 索引指针  如果两个指针对应的数相等
+for i in np.arange(1,len(dpc_time['帧计数'])):
+    foo = dpc_time.loc[i, '帧计数'] - dpc_time.loc[pt_index, '帧计数']
+    if foo == 1 : # 等于1 连续 更新指针
+        pt_index = i
+    else:  # 不等于1 有回卷 删行 不更新指针
+        dpc_time.drop(i, axis=0, inplace=True)
+        print(f'DPC有回卷 帧计数指针为{i} 连续指针为{pt_index}')        
+print(f'--------DPC回卷处理完成 最后指针为{pt_index}--------')
+
 dpc_time = dpc_time[(dpc_time['一圈内图像序号'] % 2 ) == 0 ]
 
 # 时间码计算
@@ -47,6 +67,7 @@ dpc_time['DPC时间标签'] = dpc_time['GPS秒脉冲整秒时间码'] + dpc_time
 dpc_time.index = np.arange(len(dpc_time['DPC时间标签']))
 
 # dpc_time.to_csv('time_dpc-'+now+'.csv', index=False, encoding='GB2312')
+print('--------DPC数据处理完成 POSP数据处理开始--------')
 
 # --------POSP数据计算--------
 # 切片取出用于计算的数据
@@ -56,8 +77,32 @@ posp_time = pd_posp.loc[:, ['轨道计数', '圈计数', '当前工作模式', '
 posp_time = posp_time[posp_time['轨道计数']  == orbit_posp]
 # posp_time = posp_time.iloc[start_line: , :]
 
-# 重新索引 以便自动对齐整秒部分
+# 重新索引 以便去除回卷数据 并且自动对齐整秒部分
 posp_time.index = np.arange(len(posp_time['轨道计数']))
+
+# 去除回卷数据
+''' 算法说明
+i为当前圈计数指针 pt_index为上次圈计数指针 
+如果两个指针圈计数之差为1 则连续 更新上次指针 
+如果之差小于1 则表示有回卷 删除i行 pt不变 i继续增加 重新判断 
+如果之差大于1 则表示有丢帧 提示后不处理
+'''
+pt_index = 0 # 索引指针  如果两个指针对应的数相等
+for i in np.arange(1,len(posp_time['圈计数'])):
+    foo = posp_time.loc[i, '圈计数'] - posp_time.loc[pt_index, '圈计数']
+    if foo == 1 : # 等于1 连续 更新指针
+        pt_index = i
+    else:
+        posp_time.drop(i, axis=0, inplace=True)
+        print(f'POSP有回卷 圈计数指针为{i} 连续指针为{pt_index}')
+    # elif foo < 1:  # 小于1 有回卷 删行 不更新指针
+    #     posp_time.drop(i, axis=0, inplace=True)
+    #     print(f'POSP有回卷 圈计数指针为{i} 连续指针为{pt_index}')
+    # else:
+    #     print(f'POSP帧不连续 不连续指针{i}')
+print(f'--------POSP回卷处理完成  最后指针为{pt_index}--------')        
+
+
 # 如果整秒部分不一致 删除POSP首行数据重新判断 如果一致则退出循环
 for i in np.arange(len(posp_time['圈计数'])):
     if dpc_time.loc[0, 'GPS秒脉冲整秒时间码'] != posp_time.loc[i, 'GPS整秒时刻.190']:
@@ -76,7 +121,7 @@ posp_time['POSP时间标签'] = posp_time['GPS整秒时刻.190'] + posp_time['GP
 posp_time.index = np.arange(len(posp_time['POSP时间标签']))
 
 # posp_time.to_csv('time_posp-'+now+'.csv', index=False, encoding='GB2312')
-
+print('--------POSP数据处理完成--------')
 
 # 两部分数据组合 
 timpstamp = pd.concat([dpc_time, posp_time], axis=1)
